bb2dd25c6ce1508c0af218e6c797bd82a1e6deca
Minseok Chae <117037249+chaeminseok@users.noreply.github.com>
2023-03-26 16:35:10 +0900

    homework1 modify

--- homework1/homework1.js
+++ homework1/homework1.js
@@ -1,18 +1,22 @@
 const body=document.body;
-
+const h2=document.querySelector("h2");
 function toResize(){
   const width= window.innerWidth;
   if (width<900){
     body.classList.add("blue");
+    h2.innerText="blue";
     body.classList.remove("purple");
+    
 
-  }else if(1400>width && width>=900){ 
+  }else if(1200>width && width>=900){ 
     body.classList.add("purple");
+    h2.innerText="purple";
     body.classList.remove("blue");
-    body.classList.remove("black");
+    body.classList.remove("green");
     
   }else{
-    body.classList.add("black");
+    body.classList.add("green");
+    h2.innerText="green";
     body.classList.remove("purple");
   }
 }
